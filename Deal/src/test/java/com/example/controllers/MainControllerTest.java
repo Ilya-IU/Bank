cc0a439242fcec07ff9942167e4ebf3ebe1723d2
Ilya-IU <ustyugov-1999@bk.ru>
2025-02-25 02:57:31 +0500

    Конфигурационные файлы вернул, информацию скрыл в env файлах.

--- Deal/src/test/java/com/example/controllers/MainControllerTest.java
+++ Deal/src/test/java/com/example/controllers/MainControllerTest.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -36,8 +37,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-//@TestPropertySource("/application-test.yml")
-@SpringBootTest//(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+@SpringBootTest
+@TestPropertySource("classpath:.env")
 @AutoConfigureMockMvc
 class MainControllerTest {
 
