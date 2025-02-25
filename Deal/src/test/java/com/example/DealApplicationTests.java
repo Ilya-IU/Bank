029065ec58c6a734addfb96c700e2318149542a3
Ilya-IU <ustyugov-1999@bk.ru>
2025-02-25 08:07:24 +0500

    Исправлен файл app-test.yml deal V1.2

--- Deal/src/test/java/com/example/DealApplicationTests.java
+++ Deal/src/test/java/com/example/DealApplicationTests.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource("classpath:.env")
 class DealApplicationTests {
 
     @Test
